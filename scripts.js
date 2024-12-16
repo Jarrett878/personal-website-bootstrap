4f59fc712c6aa819b3bf46e3a1c29f631986500e
Jarrett878 <nancej693@my.clevelandcc.edu>
2024-12-15 19:08:45 -0500

    Add index.html, styles.css, and link jQuery scripts

--- scripts.js
+++ scripts.js
@@ -3,3 +3,25 @@ $(document).ready(function() {
         alert("Hello! Thanks for visiting my website.");
     });
 });
+$(".hover-highlight").hover(
+    function() {
+        $(this).css("color", "red");
+    },
+    function() {
+        $(this).css("color", "black");
+    }
+);
+$("#expandable h3").click(function() {
+    $("#expandable .content").slideToggle();
+});
+$(document).ready(function() {
+    let hour = new Date().getHours();
+    if (hour < 12) {
+        $("body").css("background-color", "#f0f8ff"); // Morning - Light Blue
+    } else if (hour < 18) {
+        $("body").css("background-color", "#f4a460"); // Afternoon - Sandy Brown
+    } else {
+        $("body").css("background-color", "#2c3e50"); // Evening - Dark Blue
+        $("body").css("color", "#fff"); // Light text for contrast
+    }
+});
